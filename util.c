b1af308531503689904d65df27e50b64af9a32b1
Liutsing Robert <liutsingluo@gmail.com>
2023-09-08 22:22:00 +0800

    init commit

---
+++ util.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_empty(const char str[])
+{
+    return str[0] == '\0';
+}
---
+++ util.h
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_empty(const char str[]);
